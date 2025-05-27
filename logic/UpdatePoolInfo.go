85e8bf03177e0023e413966b07db548ba4692d9f
Felix Hua <2657226735@qq.com>
2025-05-27 19:26:34 +0800

    feat(poolinfo): 通过 AccessToken 请求接口校验并修正 UID
    - 新增 getUIDByToken 方法，请求接口 /account/info 获取 UID
    - 若 binlog 中的 UID 与返回值不一致，以接口返回为准
    - 统一 UID 来源，避免账号写错

--- logic/UpdatePoolInfo.go
+++ logic/UpdatePoolInfo.go
@@ -1,13 +1,19 @@
 package logic
 
 import (
+	"encoding/json"
 	"fmt"
 	"gf2gacha/logger"
 	"gf2gacha/model"
 	"gf2gacha/preload"
 	"gf2gacha/util"
 	"github.com/pkg/errors"
+	"io"
+	"net/http"
+	"net/url"
 	"slices"
+	"strconv"
+	"strings"
 )
 
 func UpdatePoolInfo(isFull bool) (messageList []string, err error) {
@@ -17,6 +23,18 @@ func UpdatePoolInfo(isFull bool) (messageList []string, err error) {
 	}
 	if logInfo.AccessToken == "" {
 		return nil, errors.New("Access Token不存在")
+	}
+
+	uid, err := getUIDByToken(logInfo.AccessToken)
+	if err != nil {
+		// 获取 UID 失败，谨慎起见，暂不执行更新
+		return nil, errors.WithStack(err)
+	}
+
+	if logInfo.Uid != uid {
+		// UID 不同，更新临时 UID，确保不写入错误账号
+		logger.Logger.Warnf("UID不一致：binlog UID=%s，AccessToken解析 UID=%s，将以后者为准", logInfo.Uid, uid)
+		logInfo.Uid = uid
 	}
 
 	messageList = append(messageList, logInfo.Uid)
@@ -171,4 +189,63 @@ func fullUpdatePoolInfo(logInfo model.LogInfo, poolType int64) (int, error) {
 	logger.Logger.Infof("UID:%s poolType:%d 全量更新%d条数据", logInfo.Uid, poolType, updateNum)
 
 	return updateNum, nil
+}
+
+func getUIDByToken(accessToken string) (string, error) {
+	reqBody := url.Values{}
+	// 暂时写死，在binlog中可能提取到"SDK_ACCOUNT_BASE_URL":"https://gf2-zoneinfo.sunborngame.com/"
+	infoUrl := "https://gf2-zoneinfo.sunborngame.com/account/info"
+
+	request, err := http.NewRequest("POST", infoUrl, strings.NewReader(reqBody.Encode()))
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Set("Authorization", accessToken)
+
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+	defer resp.Body.Close()
+
+	respBodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+	var respBody model.ResponseBody
+	err = json.Unmarshal(respBodyBytes, &respBody)
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+	if respBody.Code != 0 {
+		return "", errors.Errorf("%s(Code %d)", respBody.Message, respBody.Code)
+	}
+
+	var raw map[string]interface{}
+	err = json.Unmarshal(respBody.Data, &raw)
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+
+	logger.Logger.Infof("%+v\n", raw)
+	uidValue, ok := raw["uid"]
+	if !ok {
+		return "", errors.New("未找到 uid 字段")
+	}
+
+	// 强制转换为字符串
+	uid := ""
+	switch v := uidValue.(type) {
+	case string:
+		uid = v
+	case float64:
+		uid = fmt.Sprintf("%.0f", v) // 去除小数点，转换为字符串
+	case int:
+		uid = strconv.Itoa(v)
+	default:
+		return "", errors.New("uid 字段格式无法识别，无法转换为字符串")
+	}
+
+	return uid, nil
 }
